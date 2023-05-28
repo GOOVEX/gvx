f7d748a64ab11172b1e5f5e2c236ade284503b43
GOOVEX <134889143+GOOVEX@users.noreply.github.com>
2023-05-28 23:18:41 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // Set up the email details
+    $to = "goovex1gvx@gamil.com";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+    $headers = "From: $email";
+
+    // Send the email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you for your message. We will be in touch shortly.";
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
+}
+?>
